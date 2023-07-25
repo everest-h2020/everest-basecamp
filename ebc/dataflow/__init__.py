9782e1b2ba007b949d2a5e1d41b17bcb02298057
Felix Suchert <dev@felixsuchert.de>
2023-07-25 14:35:06 +0200

    remove ohuac path, move to json

--- ebc/dataflow/__init__.py
+++ ebc/dataflow/__init__.py
@@ -5,8 +5,7 @@ module = Ohua
 identifier = 'dataflow'
 
 docstrs = {'usage': 'dataflow <input-file> -o <path-to-output> --target <target> --threads <num> '
-                    '--enable-parallelism <bool> --c-limit <num> --amorphous <bool> '
-                    '--ohuac <path-to-ohuac>',
+                    '--enable-parallelism <bool> --c-limit <num> --amorphous <bool> ',
            'commands': ('dataflow', 'Invokes the dataflow flow of the EVEREST SDK.'),
            'options': [
                ('-o <path-to-output>', 'Path to save generated files under (defualts to `generated`).'),
@@ -15,5 +14,4 @@ docstrs = {'usage': 'dataflow <input-file> -o <path-to-output> --target <target>
                ('--enable-parallelism <bool>', 'Whether to enable the parallelization optimization (defaults to `true`).'),
                ('--c-limit <num>', 'Number of maximum collisions for a computation with amorphous data parallelism.'),
                ('--amorphous <bool>', 'Whether to enable the transformation of amorphous data parallel tasks (defaults to `false`).'),
-               ('--ohuac <path-to-ohuac>', 'ohuac binary to use. Can be omitted if `ohuac` is in your path.')
             ]}
