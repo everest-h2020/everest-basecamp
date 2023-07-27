4b394fa3e3312ff5b36326f7a96574da5df3e336
Burkhard Ringlein <ngl@zurich.ibm.com>
2023-07-27 11:17:03 +0200

    replaced Karls placeholder with a better placeholder

--- ebc/hpc/__init__.py
+++ ebc/hpc/__init__.py
@@ -4,6 +4,6 @@ from .cfdlang import Cfdlang
 module = Cfdlang
 identifier = 'hpc'
 
-docstrs = {'usage': 'hpc <whatever>',
+docstrs = {'usage': 'hpc [--lang <lang-id> --pipeline <pipeline> (-D <define>...) (-I <include>...)] <input> -o <path-to-output>',
            'commands': ('hpc', 'Invokes the HPC flow of the EVEREST SDK.'),
            'options': []}
